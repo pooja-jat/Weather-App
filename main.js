926b5ed851fcd8a4cff6e6585de824089ca568f5
Pooja jat <patelpoojajat@gmail.com>
2025-02-19 23:18:56 +0530

    updated

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 const btn = document.querySelector("button");
 
 function fetchWeather() {
-  const input = document.getElementById("input");
-  btn.disabled = true;
+ 
   fetch(
     `https://api.weatherapi.com/v1/current.json?key=eed6b9357579403083e160411251802&q=${input.value}&aqi=yes`
   )
     .then((res) => res.json())
     .then((data) => {
-      document.getElementById("city").innerText = data.location?.name;
-      document.getElementById("temp").innerText = data.current.temp_c + `ºC`;
+      document.getElementById("city").innerText = data.location.name;
+      document.getElementById("temp").innerText =
+        parseInt(data.current.temp_c) + `ºC`;
+      document.getElementById("condition_text").innerText =
+        data.current.condition.text;
+      document.getElementById("wind_speed").innerText =
+        data.current.wind_kph + ` kmph`;
+      document.getElementById("wind_direction").innerText =
+        data.current.wind_dir + ``;
+      document.getElementById("co").innerText = data.current.air_quality.co;
+      document.getElementById("no2").innerText = data.current.air_quality.no2;
+      document.getElementById("o3").innerText = data.current.air_quality.o3;
+      document.getElementById("so2").innerText = data.current.air_quality.so2;
     })
     .catch(() => {
       document.getElementById("city").innerText = "No matching location found";
@@ -17,7 +27,7 @@ function fetchWeather() {
     })
     .finally(() => {
       input.value = "";
-      btn.disabled = false;
+      
     });
 }
 
